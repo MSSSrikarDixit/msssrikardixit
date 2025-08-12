17de5918bac9e5d9fb458e284ef6a10bab14850a
M. S. S. Srikar Dixit <msrikarsdixitsofficials@gmail.com>
2025-08-12 13:41:45 +0530

    Add files via upload

---
+++ src/data/certificates_safe.ts
@@ -0,0 +1,94 @@
+export interface Certificate {
+  id: string;
+  title: string;
+  issuer: string;
+  date: string;
+  link: string;
+}
+
+export const certificates: Certificate[] = [
+  {
+    id: 'c1',
+    title: 'Introduction to R',
+    issuer: 'DataCamp',
+    date: '2023-02-15',
+    link: 'https://drive.google.com/drive/folders/REPLACE_WITH_YOUR_FOLDER_ID'
+  },
+  {
+    id: 'c2',
+    title: 'Introduction to TensorFlow in Python',
+    issuer: 'DataCamp',
+    date: '2023-03-12',
+    link: 'https://drive.google.com/drive/folders/REPLACE_WITH_YOUR_FOLDER_ID'
+  },
+  {
+    id: 'c3',
+    title: 'Hacking Biology for Nanotechnology',
+    issuer: 'FutureLearn',
+    date: '2023-04-10',
+    link: 'https://drive.google.com/drive/folders/REPLACE_WITH_YOUR_FOLDER_ID'
+  },
+  {
+    id: 'c4',
+    title: 'Rewriting the Code of Life with CRISPR',
+    issuer: 'FutureLearn',
+    date: '2023-05-08',
+    link: 'https://drive.google.com/drive/folders/REPLACE_WITH_YOUR_FOLDER_ID'
+  },
+  {
+    id: 'c5',
+    title: 'A Practical Guide for SARS-CoV-2 Whole Genome Sequencing',
+    issuer: 'Wellcome Connecting Science',
+    date: '2023-06-02',
+    link: 'https://drive.google.com/drive/folders/REPLACE_WITH_YOUR_FOLDER_ID'
+  },
+  {
+    id: 'c6',
+    title: 'Artificial Intelligence',
+    issuer: 'FutureLearn',
+    date: '2023-06-15',
+    link: 'https://drive.google.com/drive/folders/REPLACE_WITH_YOUR_FOLDER_ID'
+  },
+  {
+    id: 'c7',
+    title: 'Bioinformatics for Biologists: Analysing and Interpreting Genomics Datasets',
+    issuer: 'Wellcome Connecting Science',
+    date: '2023-07-03',
+    link: 'https://drive.google.com/drive/folders/REPLACE_WITH_YOUR_FOLDER_ID'
+  },
+  {
+    id: 'c8',
+    title: 'Interpreting Genomic Variation: Overcoming Challenges in Diverse Populations',
+    issuer: 'Wellcome Connecting Science',
+    date: '2023-07-12',
+    link: 'https://drive.google.com/drive/folders/REPLACE_WITH_YOUR_FOLDER_ID'
+  },
+  {
+    id: 'c9',
+    title: 'Linux for Bioinformatics',
+    issuer: 'Wellcome Connecting Science',
+    date: '2023-07-22',
+    link: 'https://drive.google.com/drive/folders/REPLACE_WITH_YOUR_FOLDER_ID'
+  },
+  {
+    id: 'c10',
+    title: 'Making Sense of Genomic Data: COVID-19 Web-based Bioinformatics',
+    issuer: 'Wellcome Connecting Science',
+    date: '2023-08-01',
+    link: 'https://drive.google.com/drive/folders/REPLACE_WITH_YOUR_FOLDER_ID'
+  },
+  {
+    id: 'c11',
+    title: 'Pathogen Genomics: A New Era in Global Health Surveillance and Strategy',
+    issuer: 'Wellcome Connecting Science',
+    date: '2023-08-12',
+    link: 'https://drive.google.com/drive/folders/REPLACE_WITH_YOUR_FOLDER_ID'
+  },
+  {
+    id: 'c12',
+    title: 'Research Methods: A Practical Guide to Peer and Community Research',
+    issuer: 'FutureLearn',
+    date: '2023-08-20',
+    link: 'https://drive.google.com/drive/folders/REPLACE_WITH_YOUR_FOLDER_ID'
+  }
+];
